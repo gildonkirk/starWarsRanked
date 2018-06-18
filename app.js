7539f95454935c35d6f366440f4f200dca261078
Gildon Kirk <gildonkirk@gmail.com>
2018-06-18 17:28:23 -0400

    movie goes to ranking list, but click function doesnt turn off until second click

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
+const movies = ['Star Wars', 'Empire Strikes Back', 'Return of the Jedi', 'The Phantom Menace', 'Attack of the Clones', 'Revenge of the Sith', 'The Force Awakens', 'Rogue One', 'The Last Jedi', 'Solo'];
+const trilogies = ['Star Wars', 'Empire Strikes Back', 'Return of the Jedi', 'The Phantom Menace', 'Attack of the Clones', 'Revenge of the Sith', 'The Force Awakens', 'The Last Jedi'];
+
+$(document).on('click', '.movie', function() {
+  $(this).clone().appendTo('.rankList');
+  $(this).removeClass('movie');
+  $(this).addClass('ranking');
+})
+
 const omdbKey = '400f5810';
+let movieTitle = 'Star+Wars';
 
 $.ajax({
-  url: `http://www.omdbapi.com/?t=the+revenant&y=&plot=short&apikey=${omdbKey}`,
+  url: `http://www.omdbapi.com/?apikey=${omdbKey}&t=${movieTitle}`,
   method: "GET"
 }).done(function(response) {
   console.log(response);
