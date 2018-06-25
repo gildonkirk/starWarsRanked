9abf8ff634eb31967471fcb3db5e164a7f63c8a3
Gildon Kirk <gildonkirk@gmail.com>
2018-06-25 12:38:59 -0400

    movie ids changed to classes, other class naming changes

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const movies = ['Star Wars', 'Empire Strikes Back', 'Return of the Jedi', 'The P
 const trilogies = ['Star Wars', 'Empire Strikes Back', 'Return of the Jedi', 'The Phantom Menace', 'Attack of the Clones', 'Revenge of the Sith', 'The Force Awakens', 'The Last Jedi'];
 const ranking = [];
 $(document).on('click', '.movie', function() {
-  $(this).appendTo('.rankList');
   $(this).removeClass('movie');
   $(this).addClass('ranking');
+  $(this).clone().appendTo('.rankList');
+  $(this).removeClass('ranking');
+  $(this).addClass('ranked');
   ranking.push(this.innerText || this.textContent);
 });
 
+if(ranking.length > 9) {
+
+}
 const omdbKey = '400f5810';
 let movieTitle = 'Star+Wars';
 
