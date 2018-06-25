6b4b6f4f984887616e88657875e0e810112cdadd
Gildon Kirk <gildonkirk@gmail.com>
2018-06-25 12:29:16 -0400

    user ranking pushed to ranking array on each click

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const movies = ['Star Wars', 'Empire Strikes Back', 'Return of the Jedi', 'The Phantom Menace', 'Attack of the Clones', 'Revenge of the Sith', 'The Force Awakens', 'Rogue One', 'The Last Jedi', 'Solo'];
 const trilogies = ['Star Wars', 'Empire Strikes Back', 'Return of the Jedi', 'The Phantom Menace', 'Attack of the Clones', 'Revenge of the Sith', 'The Force Awakens', 'The Last Jedi'];
-
+const ranking = [];
 $(document).on('click', '.movie', function() {
   $(this).appendTo('.rankList');
   $(this).removeClass('movie');
   $(this).addClass('ranking');
-})
+  ranking.push(this.innerText || this.textContent);
+});
 
 const omdbKey = '400f5810';
 let movieTitle = 'Star+Wars';
