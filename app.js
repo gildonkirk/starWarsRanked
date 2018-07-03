6f27a70d7363a3fa7185f01e20ee8bfd98c2a301
Gildon Kirk <gildonkirk@gmail.com>
2018-07-03 12:36:40 -0400

    list appends after user rankings

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 const movies = ['Star Wars', 'Star Wars: Episode V - The Empire Strikes Back', 'Star Wars: Episode VI - Return of the Jedi', 'Star Wars: Episode I - The Phantom Menace', 'Star Wars: Episode II - Attack of the Clones', 'Star Wars: Episode III - Revenge of the Sith', 'The Force Awakens', 'Rogue One: A Star Wars Story', 'The Last Jedi', 'Solo: A Star Wars Story'];
 let data = [];
-let nums = [1,40,8];
+let userRanking = [];
+let rottenTomatoesRanking = [];
 // function Movie(name, date, rank) {
 //   this.name = name;
 //   this.releaseDate = date;
 //   this.ranking = rank;
 // }
 
-const trilogies = ['Star Wars', 'Empire Strikes Back', 'Return of the Jedi', 'The Phantom Menace', 'Attack of the Clones', 'Revenge of the Sith', 'The Force Awakens', 'The Last Jedi'];
-const ranking = [];
 $(document).on('click', '.movie', function() {
   $(this).removeClass('movie');
   $(this).addClass('ranking');
   $(this).clone().appendTo('.rankList');
   $(this).removeClass('ranking');
   $(this).addClass('ranked');
-  ranking.push(this.innerText || this.textContent);
-  if(ranking.length > 9) {
+  userRanking.push(this.innerText || this.textContent);
+  if(userRanking.length > 9) {
     $('.movieList').remove();
     $('.movieListHeader').remove();
+    rottenRanking();
+    $('.rankList').after('<ol class="rotten"><li>Hey</li></ol>');
   }
 });
 
@@ -32,29 +33,33 @@ let movieTitle = 'Star+Wars';
 //   console.log(response);
 // });
 
-for(i = 0; i < movies.length; i++) {
-  movieTitle = movies[i].replace(/ /g, '+');
-  $.ajax({
-    url: `http://www.omdbapi.com/?apikey=${omdbKey}&t=${movieTitle}`,
-    method: "GET"
-  }).done(function(response) {
-    // console.log(response);
-    data.push(response);
-    if(data.length > 9) {
-      data.sort(movieSort);
-      console.log(data);
-    };
-  });
-}
+
+
+function rottenRanking() {
+  for(i = 0; i < movies.length; i++) {
+    movieTitle = movies[i].replace(/ /g, '+');
+    $.ajax({
+      url: `http://www.omdbapi.com/?apikey=${omdbKey}&t=${movieTitle}`,
+      method: "GET"
+    }).done(function(response) {
+      // console.log(response);
+      data.push(response);
+      if(data.length > 9) {
+        data.sort(movieSort);
+        console.log(data);
+      };
+    });
+  };
+};
 
 function movieSort(a, b) {
-  let yearA = a.Year;
-  let yearB = b.Year;
+  let yearA = a.Ratings[1].Value;
+  let yearB = b.Ratings[1].Value;
   let comparison = 0;
   if(yearA > yearB) {
-    comparison = 1;
-  } else if(yearA < yearB) {
     comparison = -1;
+  } else if(yearA < yearB) {
+    comparison = 1;
   }
   return comparison;
 }
