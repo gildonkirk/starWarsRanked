6f3861cf443ecdaeeccc795a9043cfe3f391b734
Gildon Kirk <gildonkirk@gmail.com>
2018-07-03 12:09:03 -0400

    sorts movies by release date

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const movies = ['Star Wars', 'Star Wars: Episode V - The Empire Strikes Back', 'Star Wars: Episode VI - Return of the Jedi', 'Star Wars: Episode I - The Phantom Menace', 'Star Wars: Episode II - Attack of the Clones', 'Star Wars: Episode III - Revenge of the Sith', 'The Force Awakens', 'Rogue One: A Star Wars Story', 'The Last Jedi', 'Solo: A Star Wars Story'];
-let moviesRanked = {
-
-}
+let data = [];
+let nums = [1,40,8];
+// function Movie(name, date, rank) {
+//   this.name = name;
+//   this.releaseDate = date;
+//   this.ranking = rank;
+// }
 
 const trilogies = ['Star Wars', 'Empire Strikes Back', 'Return of the Jedi', 'The Phantom Menace', 'Attack of the Clones', 'Revenge of the Sith', 'The Force Awakens', 'The Last Jedi'];
 const ranking = [];
@@ -21,20 +25,40 @@ $(document).on('click', '.movie', function() {
 const omdbKey = '400f5810';
 let movieTitle = 'Star+Wars';
 
-$.ajax({
-  url: `http://www.omdbapi.com/?apikey=${omdbKey}&t=${movieTitle}`,
-  method: "GET"
-}).done(function(response) {
-  console.log(response);
-});
+// $.ajax({
+//   url: `http://www.omdbapi.com/?apikey=${omdbKey}&t=${movieTitle}`,
+//   method: "GET"
+// }).done(function(response) {
+//   console.log(response);
+// });
 
 for(i = 0; i < movies.length; i++) {
   movieTitle = movies[i].replace(/ /g, '+');
-  console.log(movieTitle);
   $.ajax({
     url: `http://www.omdbapi.com/?apikey=${omdbKey}&t=${movieTitle}`,
     method: "GET"
   }).done(function(response) {
-    console.log(response);
+    // console.log(response);
+    data.push(response);
+    if(data.length > 9) {
+      data.sort(movieSort);
+      console.log(data);
+    }
   });
 }
+
+function movieSort(a, b) {
+  let yearA = a.Year;
+  let yearB = b.Year;
+  let comparison = 0;
+  if(yearA > yearB) {
+    comparison = 1;
+  } else if(yearA < yearB) {
+    comparison = -1;
+  }
+  return comparison;
+}
+
+
+
+// const newHope = new Movie(response.Title, response.Year, response.Ratings[0].Value);
